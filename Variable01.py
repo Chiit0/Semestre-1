e523e165d794a1f3406a51f1e397095dfe096bab
Chiit0 <Chiit0@users.noreply.githup.com>
2023-04-06 14:05:10 -0400

    Mi primer Commit

---
+++ Variable01.py
@@ -0,0 +1,25 @@
+#homero saludando
+print("Hola choi Homero chino")
+
+#variables aca
+nombre = "Homero"
+name = "Marge"
+
+#ejemplo en uso
+print (name)
+print ("Hola soy", nombre)
+
+#colocar edad
+edad=45
+print ("hola", name , "mi edad es de", edad)
+
+###Imprimiendo 2 varianbles en una cadena usando +
+print("hola yo soy" " " + nombre + " " + "y tengo" + " " + str(edad) + " " + "años")
+
+#sobrescribir datos
+nombre="Cosme Fulanito"
+print("Hola, ahora soy" + " " + nombre)
+
+#utilizar input
+nombre1=input("¿cual es tú nombre?\n")
+print("Tu nombre es:", nombre1)
