dedee1402309a9eda0aaf053b7683e83c7fba8fa
Chiit0 <Chiit0@users.noreply.githup.com>
2023-04-06 16:01:00 -0400

    Tipos de datos

---
+++ 02tiposdatos.py
@@ -0,0 +1,30 @@
+#VARIABLES NUMERICAS
+estatura = 1.71
+peso = 70
+complejo = 1 +4j
+
+print("Impresion del número complejo:", complejo)
+
+#OPERACIÓN ARITMETICA BASICA
+imc = peso/estatura**2
+print(imc)
+
+#02 DATOS DE TIPO CADENA DE CARACTERES
+asignatura = "Programación"
+carrera = "Ingenierría Civil en Informatíca"
+
+print("la asignatura de", asignatura, "corresponde a la carrera de", carrera)
+
+
+#VALORES BOOLEANOS
+ampolleta = False
+interruptor = True
+
+#Con type sabemos el tipo de datos
+print(type(ampolleta))
+
+#DATOS DE TIPO ARRAY
+estudiantes = ["Matias", "Marco", "Cristobal", "Sebastian"]
+num = [1,2,3,4,5,6]
+print(estudiantes)
+print(num)
