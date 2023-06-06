3b9c6974ed5185e0071ed328ec14edc5664cf047
Chiit0 <Chiit0@users.noreply.githup.com>
2023-06-06 17:03:59 -0400

    Laboratorio/Parcial

---
+++ Funciones-dvd.py
@@ -0,0 +1,16 @@
+def funcion_1():
+    print("WENA PO, version función")
+funcion_1()
+
+def concatenacion(lista1, lista2):
+    return lista1 + lista2
+
+frutas_dvd = ["manzana", "pera", "platano",1]
+frutas_rancias = ["tomate", "pepino", "morron"]
+
+print(concatenacion(frutas_dvd, frutas_rancias))
+
+def multiplicación(num1,num2):
+    return num1*num2
+
+print(multiplicación(6,9))
